dbcfa19a9dbbfbdff313346ecd6db822d3fc0127
ead2015 <ead.shuja@gmail.com>
2015-11-20 19:58:11 +0500

    Commit-1

---
+++ MyClientApplication/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClientApplication.SR1;
+namespace MyClientApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           CarRentalServiceClient myClient = new CarRentalServiceClient();
+
+            string message = myClient.GetData(13);
+            Console.WriteLine("message  = "+ message);
+
+            Car car = new Car();
+            car.BoolValue = true;
+            car.StringValue = "this is my car";
+
+           Car mySecondCar= myClient.GetDataUsingDataContract(car);
+
+           Console.WriteLine("Car String Value = "+ mySecondCar.StringValue);
+           myClient.Close();
+        }
+    }
+}
